1b46da7fda68daeec047458b2d9f251121514354
daymare <daylankelting@gmail.com>
2019-06-24 12:04:20 -0700

    modified run script to save to a unique file every run to prevent overlap on runs that run at the same time.

--- run.bash
+++ run.bash
@@ -1,6 +1,14 @@
 
-#python3 main.py
 
-python3 main.py 2>&1 | tee out.txt; vim '+ normal G zR' out.txt
+# ensure unique filename
 
+number=0
+base_name=outputs/out
+fname=$base_name.txt
+
+while [ -e "$fname" ]; do
+    printf -v fname -- '%s-%02d.txt' "$base_name" "$(( ++number ))"
+done
+
+python3 main.py 2>&1 | tee $fname; vim '+ normal G zR' $fname
 
