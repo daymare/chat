e40cc2dc35354b3b9d59915ed2bc8c7971eb27a3
daymare <daylankelting@gmail.com>
2019-07-24 16:44:05 -0700

    moved utility functions out of model into a dedicated util file

--- model.py
+++ model.py
@@ -13,49 +13,8 @@ from util.train_util import get_batch_iterator
 from util.train_util import get_loss
 from util.train_util import calculate_hidden_cos_similarity
 
-
-def lstm(units, name=None):
-    if tf.test.is_gpu_available():
-        return tf.keras.layers.CuDNNLSTM(units,
-                return_sequences=True,
-                return_state=True,
-                trainable=True,
-                name=name)
-    else:
-        return tf.keras.layers.LSTM(
-                units,
-                return_sequences=True,
-                return_state=True,
-                trainable=True,
-                name=name)
-
-def initialize_multilayer_hidden_state(layer_sizes, batch_size):
-    hidden = []
-    for layer in range(len(layer_sizes)):
-        layer_size = layer_sizes[layer]
-        layer_hidden = [
-            tf.zeros((batch_size, layer_size)),
-            tf.zeros((batch_size, layer_size))
-            ]
-        hidden.append(layer_hidden)
-
-    return hidden
-
-
-def gru(units):
-    if tf.test.is_gpu_available():
-        return tf.keras.layers.CuDNNGRU(
-                units,
-                return_sequences=True,
-                return_state=True,
-                recurrent_initializer='glorot_uniform')
-    else:
-        return tf.keras.layers.GRU(
-                units,
-                return_sequences=True,
-                return_state=True,
-                recurrent_activation='sigmoid',
-                recurrent_initializer='glorot_uniform')
+from util.model_util import lstm
+from util.model_util import initialize_multilayer_hidden_state
 
 
 class PersonaEncoder(tf.keras.Model):
