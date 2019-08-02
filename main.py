17f185a694b5d79428d499998d02854566bc12d8
daymare <daylankelting@gmail.com>
2019-08-02 12:47:09 -0700

    parameters for 2048 sample test take 2. Halved the learning rate back to original.

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ tf.app.flags.DEFINE_list('decoder_sizes', '500, 500, 500',
 tf.app.flags.DEFINE_float('max_gradient_norm',
         3.0, 'max gradient norm to clip to during training')
 tf.app.flags.DEFINE_float('learning_rate',
-        6*10**-4, 'learning rate during training')
+        3*10**-4, 'learning rate during training')
 tf.app.flags.DEFINE_integer('batch_size',
         32, 'batch size')
 
@@ -94,7 +94,7 @@ tf.app.flags.DEFINE_boolean('load_model',
         True, 
         'whether to load the model from file or not for training.')
 tf.app.flags.DEFINE_string('logdir',
-        './train/progressive_overfit/2048', 'where to save tensorboard summaries')
+        './train/progressive_overfit/2048/take2', 'where to save tensorboard summaries')
 tf.app.flags.DEFINE_integer('dataset_size', 2048, 
         'number of samples to put in the dataset. -1 indicates 90/10 train test split')
 tf.app.flags.DEFINE_bool('use_epochs', True,
