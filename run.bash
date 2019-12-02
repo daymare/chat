df55b8488015edce022d21dfd66ea450321b0cd8
daymare <daylankelting@gmail.com>
2019-12-02 12:29:49 -0800

    added notee and pdb flags to run.bash

--- run.bash
+++ run.bash
@@ -10,5 +10,20 @@ while [ -e "$fname" ]; do
     printf -v fname -- '%s-%02d.txt' "$base_name" "$(( ++number ))"
 done
 
-python3 main.py $@ 2>&1 | tee $fname; vim '+ normal G zR' $fname
+
+args=()
+for var in "$@"; do
+    # ignore the arguments specific to this bash script
+    [ "$var" != '--notee' ] && [ "$var" != '--pdb' ] && args+=("$var")
+done
+
+if [[ "$*" == *--notee* ]]
+then
+    python3 main.py "${args}" 2>&1
+elif [[ "$*" == *--pdb* ]]
+then
+    pdb3 main.py "${args}"
+else
+    python3 main.py $@ 2>&1 | tee $fname; vim '+ normal G zR' $fname
+fi
 
